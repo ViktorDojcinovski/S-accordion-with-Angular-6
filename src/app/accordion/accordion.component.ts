3c90e8f679652542fa44a27f901516a9435f841b
Viktor Dojchinovski <viktor.dojchinovski@symphony-solutions.eu>
2018-07-09 00:44:56 +0200

    Connect service Employee to Accordion component

--- src/app/accordion/accordion.component.ts
+++ src/app/accordion/accordion.component.ts
@@ -1,15 +1,70 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ElementRef } from '@angular/core';
+
+import { EmployeeService } from '../employee.service';
+import { Section } from '../employee.service';
+
+import { ButtonsModule } from 'angular-bootstrap-md';
 
 @Component({
   selector: 'app-accordion',
   templateUrl: './accordion.component.html',
-  styleUrls: ['./accordion.component.scss']
+  styleUrls: ['./accordion.component.scss'],
+  providers:  [ EmployeeService ]
 })
-export class AccordionComponent implements OnInit {
+export class AccordionComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  employees: Section[];
 
-  ngOnInit() {
+  items;
+  item_headings;
+  show_less_btns;
+
+  constructor(private service: EmployeeService, private elementRef: ElementRef) {
   }
 
+  ngOnInit() {
+    this.getEmployees();
+  }
+
+  ngAfterViewInit() {
+
+    this.items = this.elementRef.nativeElement.querySelectorAll('.list-item');
+    this.item_headings = this.elementRef.nativeElement.querySelectorAll('.list-item-heading');
+    this.show_less_btns = this.elementRef.nativeElement.querySelectorAll('.show-less');
+    this.addListenersHeadings();
+    this.addListenersShowLessButtons();
+  }
+
+  getEmployees() {
+    this.employees = this.service.getEmployees();
+  }
+
+  addListenersHeadings() {
+    for (let i = 0; i < this.item_headings.length; i++) {
+      this.item_headings[i].addEventListener('click', this.toggleMetaDescription.bind(this), false);
+    }
+  }
+
+  addListenersShowLessButtons() {
+    for (let i = 0; i < this.show_less_btns.length; i++) {
+      console.log(this.show_less_btns[i]);
+      this.show_less_btns[i].addEventListener('click', this.showLessFunc.bind(this), false);
+    }
+  }
+
+  toggleMetaDescription(event) {
+    const itemClass = event.currentTarget.parentNode.className;
+    for (let i = 0; i < this.items.length; i++) {
+      this.items[i].className = 'list-item close';
+    }
+    if (itemClass === 'list-item close') {
+        event.currentTarget.parentNode.className = 'list-item open';
+        event.currentTarget.removeEventListener('click', this.toggleMetaDescription.bind(this), false);
+    }
+  }
+
+  showLessFunc(event) {
+    event.currentTarget.parentNode.parentNode.className = 'list-item close';
+  }
 }
+
--- src/app/employee.service.ts
+++ src/app/employee.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
 
+/**
+ * Duck-type checkup with interface
+ * for the data types -- list of employees
+ */
+  export interface Section {
+    name: string;
+    position: string;
+    score: number;
+  }
+
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
 
   constructor() { }
+
+  /**
+   * Mockup data for emplyees list,
+   * it uses type interface of Section
+   */
+    private employees: Section[] = [
+      {
+        name: 'John Smith',
+        position: 'Sales Representative',
+        score: 20,
+      },
+      {
+        name: 'John Smith',
+        position: 'Sales Representative',
+        score: 20,
+      }
+    ];
+
+  /** Employees getter */
+    getEmployees() {
+      return this.employees;
+    }
 }
