165524f99b8032bc69f7971ac247060a13e2eeaa
Viktor Dojchinovski <viktor.dojchinovski@symphony-solutions.eu>
2018-07-09 00:36:14 +0200

    Create component Accordion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { AccordionComponent } from './accordion/accordion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccordionComponent
   ],
   imports: [
     BrowserModule,
