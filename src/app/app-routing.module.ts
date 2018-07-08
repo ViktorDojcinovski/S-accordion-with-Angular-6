e3ddda3ca511dddea133f226f4ccb73585ce01ff
Viktor Dojchinovski <viktor.dojchinovski@symphony-solutions.eu>
2018-07-09 00:39:54 +0200

    Configure router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+import { AccordionComponent } from './accordion/accordion.component';
+
+const routes: Routes = [
+  { path: 'accordion', component: AccordionComponent }
+];
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [ RouterModule.forRoot(routes) ],
+  exports: [ RouterModule ]
 })
+
 export class AppRoutingModule { }
